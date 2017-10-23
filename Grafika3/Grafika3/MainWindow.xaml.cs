87728d85ffb6dcc70dd3454614dc39578201e6a5
Marcin Bogdan <boddzio1@interia.pl>
2017-10-23 23:31:46 +0200

    RGB cube done

---
+++ Grafika3/Grafika3/MainWindow.xaml.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using SharpDX;
+using SharpDX.Direct3D9;
+using SharpDX.Windows;
+using Color = SharpDX.Color;
+using MouseEventArgs = System.Windows.Forms.MouseEventArgs;
+
+namespace Grafika3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.Windows.Point _mouseStart;
+        private bool _mouseCaptured;
+        private Form _form;
+        private float _rotationX;
+        private float _rotationY;
+        private float _rotationZ;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _rotationX = 1;
+            _rotationY = 1;
+            _rotationZ = 0;
+
+            _form = new Form();
+            _form.TopLevel = false;
+            _form.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            _form.MouseDown += FormOnMouseDown;
+            _form.MouseUp += FormOnMouseUp;
+            _form.MouseMove += FormOnMouseMove;
+            wfHost.Child = _form;
+            RenderCube();
+        }
+
+        private void MainWindow_OnUnloaded(object sender, RoutedEventArgs e)
+        {
+            _form.Dispose();
+        }
+
+        private void FormOnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_mouseCaptured) return;
+
+            System.Windows.Point curContentMousePoint = new System.Windows.Point(e.Location.X, e.Location.Y);
+            var x = (float)curContentMousePoint.X - (float)_mouseStart.X;
+            var y = (float)curContentMousePoint.Y - (float)_mouseStart.Y;
+            if (x <= -1 && y <= -1)
+            {
+                _rotationX += 0.01f;
+                _rotationY += 0.01f;
+            }
+            else if (x >= 1 && y >= 1)
+            {
+                _rotationX -= 0.01f;
+                _rotationY -= 0.01f;
+
+            }
+            else if (x >= 1 && y <= -1)
+            {
+                _rotationX += 0.01f;
+                _rotationY -= 0.01f;
+
+            }
+            else if (x <= -1 && y >= 1)
+            {
+                _rotationX -= 0.01f;
+                _rotationY += 0.01f;
+
+            }
+
+            _mouseStart = curContentMousePoint;
+        }
+
+        private void FormOnMouseUp(object sender, MouseEventArgs e)
+        {
+            if (_mouseCaptured)
+            {
+                _mouseCaptured = false;
+            }
+        }
+
+        private void FormOnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                _mouseStart = new System.Windows.Point(e.Location.X, e.Location.Y);
+                _mouseCaptured = true;
+            } 
+        }
+
+        private void RenderCube()
+        {
+            // Creates the Device
+            var direct3D = new Direct3D();
+            var device = new Device(direct3D, 0, DeviceType.Hardware, _form.Handle, CreateFlags.HardwareVertexProcessing, new PresentParameters(_form.Width, _form.Height));
+
+            // Creates the VertexBuffer
+            var vertices = new VertexBuffer(device, Utilities.SizeOf<Vector4>() * 2 * 36, Usage.WriteOnly, VertexFormat.None, Pool.Managed);
+            vertices.Lock(0, 0, LockFlags.None).WriteRange(new[]
+            {
+                new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f), // Front
+                new Vector4(-1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 1.0f, 1.0f, 1.0f),
+
+                new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 1.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+
+                new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 0.0f, 0.0f, 1.0f), // BACK
+                new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+
+                new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+
+                new Vector4(-1.0f, 1.0f, -1.0f,  1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f), // Top
+                new Vector4(-1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+
+                new Vector4(-1.0f, 1.0f, -1.0f,  1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f, 1.0f,  1.0f,  1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f, 1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 1.0f, 1.0f),
+
+                new Vector4(-1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f), // Bottom
+                new Vector4( 1.0f,-1.0f,  1.0f,  1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4(-1.0f,-1.0f,  1.0f,  1.0f), new Vector4(0.0f, 0.0f, 0.0f, 1.0f),
+
+                new Vector4(-1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f,-1.0f, -1.0f,  1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f,-1.0f,  1.0f,  1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+
+                new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f), // Left
+                new Vector4(-1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+
+                new Vector4(-1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4(-1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 0.0f, 1.0f, 1.0f),
+                new Vector4(-1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 0.0f, 1.0f, 1.0f),
+
+                new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f), // Right
+                new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f, -1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 0.0f, 1.0f),
+
+                new Vector4( 1.0f, -1.0f, -1.0f, 1.0f), new Vector4(1.0f, 1.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f,  1.0f, -1.0f, 1.0f), new Vector4(1.0f, 1.0f, 1.0f, 1.0f),
+                new Vector4( 1.0f,  1.0f,  1.0f, 1.0f), new Vector4(0.0f, 1.0f, 1.0f, 1.0f),
+            });
+            vertices.Unlock();
+
+            // Compiles the effect
+            var effect = SharpDX.Direct3D9.Effect.FromFile(device, "MiniCube.fx", ShaderFlags.None);
+
+            // Allocate Vertex Elements
+            var vertexElems = new[] {
+                new VertexElement(0, 0, DeclarationType.Float4, DeclarationMethod.Default, DeclarationUsage.Position, 0),
+                new VertexElement(0, 16, DeclarationType.Float4, DeclarationMethod.Default, DeclarationUsage.Color, 0),
+                VertexElement.VertexDeclarationEnd
+            };
+
+            // Creates and sets the Vertex Declaration
+            var vertexDecl = new VertexDeclaration(device, vertexElems);
+            device.SetStreamSource(0, vertices, 0, Utilities.SizeOf<Vector4>() * 2);
+            device.VertexDeclaration = vertexDecl;
+
+            // Get the technique
+            var technique = effect.GetTechnique(0);
+            var pass = effect.GetPass(technique, 0);
+
+            // Prepare matrices
+            var view = Matrix.LookAtLH(new Vector3(0, 0, -5), new Vector3(0, 0, 0), Vector3.UnitY);
+            var proj = Matrix.PerspectiveFovLH((float)Math.PI / 4.0f, _form.Width / (float)_form.Height, 0.1f, 100.0f);
+            var viewProj = Matrix.Multiply(view, proj);
+
+            // Use clock
+            var clock = new Stopwatch();
+            clock.Start();
+
+            RenderLoop.Run(_form, () =>
+            {
+                device.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Black, 1.0f, 0);
+                device.BeginScene();
+
+                effect.Technique = technique;
+                effect.Begin();
+                effect.BeginPass(0);
+
+                var worldViewProj = Matrix.RotationX(_rotationX) * Matrix.RotationY(_rotationY) * Matrix.RotationZ(_rotationZ) * viewProj;
+                effect.SetValue("worldViewProj", worldViewProj);
+
+                device.DrawPrimitives(PrimitiveType.TriangleList, 0, 12);
+
+                effect.EndPass();
+                effect.End();
+
+                device.EndScene();
+                device.Present();
+            });
+
+            effect.Dispose();
+            vertices.Dispose();
+            device.Dispose();
+            direct3D.Dispose();
+        }
+    }
+}
